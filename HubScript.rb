e8f820329e860826dab23c56df3cd62eefbddb52
jorgegene <jorgegene@hotmail.es>
2019-12-15 19:21:27 +0100

    Menu v2

--- HubScript.rb
+++ HubScript.rb
@@ -1,29 +1,36 @@
 #!/usr/bin/ruby
 
 require 'colorize'
+require 'snmp'
 
-fin = false
+@host = 
 
 
+def ListAllPorts()
 
-option1 = "1) List all ports.\n"
-option2 = "2) Change port to a segment.\n"
-option3 = "3) Exit"
 
-menu ="Select operation number:\n"+option1+option2+option3
+begin
+    fin = false
 
-while fin == false do
-    puts menu.yellow
-    option = gets.chomp
-    if option == "1" then
+    option1 = "1) List all ports.\n"
+    option2 = "2) Change port to a segment.\n"
+    option3 = "3) Exit"
 
-    elsif option == "2" then
-        
-    elsif option == "3" then
-        puts "Cerrando programa"
-        exit(0)
-    else
-        puts "Incorrect option, use a valid number".red
+    menu ="Select operation number:\n"+option1+option2+option3
+
+    while fin == false do
+        puts menu.yellow
+        option = gets.chomp
+        if option == "1" then
+
+        elsif option == "2" then
+            
+        elsif option == "3" then
+            puts "Cerrando programa"
+            fin = true
+        else
+            puts "Incorrect option, use a valid number".red
+        end
+
     end
-
 end
