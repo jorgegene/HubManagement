f558c3a3504cef8cdbecc71e8af01c3ee65b1824
jorgegene <jorgegene@hotmail.es>
2019-12-15 21:34:31 +0100

    commit

--- HubScript.rb
+++ HubScript.rb
@@ -19,7 +19,7 @@ def ValidPort?(port)
     if port.is_a? Integer then
         if port >= 1 && port <= 14 then
             if port == 12
-                puts "Port 12 not allowed"
+                puts "Port 12 not allowed".red
                 return false
             else
                 return true
@@ -30,6 +30,11 @@ def ValidPort?(port)
 end
 
 def BandwithOnPort(port)
+    manager = Client.new(:host => @host,:community => 'security',:version => :SNMPv1)
+    oid = "1.3.6.1.4.1.171.11.113.1.3.2.2.1.1.5."+port
+    manager.walk(oid: oid).each do |oid_code, value|
+        puts "for #{oid_code}: #{value}"
+    end
 end
 
 begin
@@ -52,19 +57,19 @@ begin
         elsif option == "3" then
             dentro = false
             while dentro == false do
-                puts "Select port to see Bandwith [1-14]. 0 to go back to the Menu"
+                puts "Select port to see Bandwith [1-14]. 0 to go back to the Menu".green
                 port = gets.chomp
                 if port == "0" then
                     dentro = true
-                elsif ValidPort?(port) then
+                elsif ValidPort?(port.to_i) then
                    BandwithOnPort(port)
                 else
-                    puts "Incorrect port"
+                    puts "Incorrect port".red
                 end
             end
 
         elsif option == "4" then
-            puts "Closing program"
+            puts "Closing program".yellow
             fin = true
         else
             puts "Incorrect option, use a valid number".red
