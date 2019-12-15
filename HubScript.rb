bd406d883d3f669bc78307efb9ff44dadb2fbafe
pepelongares <josefelix_96@hotmail.com>
2019-12-15 23:09:06 +0100

    Segment change wip

--- HubScript.rb
+++ HubScript.rb
@@ -27,25 +27,38 @@ def ListAllPorts(segmentIds)
 end
 
 def ValidPort?(port)
-    if port.is_a? Integer then
-        if port >= 1 && port <= 14 then
-            if port == 12
-                puts "Port 12 not allowed"
-                return false
-            else
-                return true
-            end
-        end
-    end
-    return false
+  portI = port.to_i
+  if port >= 1 && port <= 14 then
+      if port == 12
+          puts "Port 12 not allowed"
+          return false
+      else
+          return true
+      end
+  end
+  return false
 end
 
 def BandwithOnPort(port)
 
 end
 
-def ChangePort2NewSegment(port)
-
+def ChangePort2NewSegment(port, segmentIds)
+  dentro = false
+  while dentro == false do
+      puts "Select new segment [1-4] for port" + port + "or 0 to go back to the port select"
+      segment = gets.chomp
+      segment = segment.to_i
+      if segment == "0" then
+        dentro = true
+      elsif segment > 0 && segment < 4 then
+        manager = Client.new(:host => @host,:community => 'security',:version => :SNMPv1)
+        manager.set("1.3.6.1.4.1.43.10.26.1.1.1.5"+port, value: segmentIds.at(segment-1))
+        manager.close
+      else
+          puts "Incorrect segment\n".red
+      end
+  end
 end
 
 def GetSegmentIds()
@@ -78,6 +91,7 @@ begin
         option = gets.chomp
         if option == "1" then # List all the ports
             ListAllPorts(segmentIds)
+
         elsif option == "2" then  # Change port to different segment
           dentro = false
           while dentro == false do
@@ -86,7 +100,7 @@ begin
               if port == "0" then
                   dentro = true
               elsif ValidPort?(port) then
-                 ChangePort2NewSegment(port)
+                 ChangePort2NewSegment(port, segmentIds)
               else
                   puts "Incorrect port\n".red
               end
